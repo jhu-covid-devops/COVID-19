6e2294a1f8ec8bfceda14106833408de92c92cc5
Ryan Lau <rylllau@gmail.com>
2020-11-17 14:46:11 -0500

    Update create.py

--- .github/workflows/create.py
+++ .github/workflows/create.py
@@ -6,7 +6,8 @@ token = os.getenv('GITHUB_TOKEN', str(sys.argv[1]))
 g = Github(token)
 repo = g.get_repo("jhu-covid-devops/COVID-19")
 
-branch_name = str(sys.argv[2]) + '_' + str(sys.argv[3]).replace(" ","_")
+branch_name = str(sys.argv[2]) + '_' + str(sys.argv[3])
+branch_name = branch_name.replace(" ","_")
 
 source = repo.get_branch("master")
 repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=source.commit.sha)
